aca967f1fff1ea871589315360329e75d0c109b5
Lori Kim <kim.lori57@gmail.com>
2019-04-14 16:58:57 -0400

    commit message

---
+++ README.md
@@ -0,0 +1,15 @@
+# spring_2019_team1
+
+
+Instructions:
+
+1. Navigate to /src
+
+2. run following commands:
+    a) composer install
+    b) cp .env.example .env 
+    c) php artisan key:generate 
+    d) php artisan serve
+    
+3. Open browser and navigate to localhost:8000/
+    
---
+++ src/resources/views/bootstrapgrids.blade.php
@@ -0,0 +1,42 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <title>Bootstrap Example</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    
+
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+
+    <!-- jQuery library -->
+    <script src="https://ajax.googleapis.com.ajax.libs/jquery/3.3.1/jquery.min.js"></script>
+
+    <!-- Latest compiled JavaScript -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
+
+</head>
+
+<body>
+
+<div class="container-fluid">
+    <h1>My Bootstrap Page (with a fluid container)</h1>
+    <p>This part is inside a .container-fluid class.</p>
+    <p>The .container-fluid class provides a full width container, spanning the entire width of the viewport.</p>
+    <p>Resize the browser window to see the effect.</p>
+    <p>The columns will automatically stack on top of each other when the screen is less than 768px wide.</p>
+
+    <div class="row">
+        <div class="col-sm-4" style="background-color:lavender;">.col-sm-4</div>
+        <div class="col-sm-4" style="background-color:lavenderblush;">.col-sm-4</div>
+        <div class="col-sm-4" style="background-color:cyan;">.col-sm-4</div>
+    </div>
+
+    <div class="row">
+        <div class="col-sm-8" style="background-color:lavender;">.col-sm-8</div>
+        <div class="col-sm-4" style="background-color:lavenderblush;">.col-sm-4</div>
+</div>
+
+</body>
+</html>
