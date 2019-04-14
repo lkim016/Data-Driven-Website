811c3d26d899a03852f5004b34dfb82dd4e9df34
Lori Kim <kim.lori57@gmail.com>
2019-04-14 14:15:55 -0400

    Changing the file order format in github.

---
+++ Phase 1/Project/resources/views/bootstrap.blade.php
@@ -0,0 +1,30 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <title>Bootstrap Example</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    
+
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+
+    <!-- jQuery library -->
+    <script src="https://ajax.googleapis.com.ajax.libs/jquery/3.3.1/jquery.min.js"></script>
+
+    <!-- Latest compiled JavaScript -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
+
+</head>
+
+<body>
+
+<div class="container">
+    <h1>My Bootstrap Page !</h1>
+    <p>This part is inside a .container class.</p>
+    <p>The .container class provides a responsive fixed width container.</p>
+</div>
+
+</body>
+</html>
---
+++ README.md
@@ -0,0 +1,11 @@
+# spring_2019_team1
+
+
+Instructions:
+
+1. Navigate to root directory of project
+
+2. run following commnds: a) composer install b) cp .env.example .env c) php artisan key:generate d) php artisan serve
+    
+3. Navigate to localhost:8000/
+    
