0fb9bc953b70bad38c061ef32263227ebaf4bf28
joe <joemoondev@gmail.com>
2019-04-19 00:20:33 -0700

    Added login & main view with css

--- src/resources/views/login.blade.php
+++ src/resources/views/login.blade.php
@@ -1,177 +1,43 @@
 <!doctype html>
 <html>
 <head>
-<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="css/style.css">
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
 <style>
-body {font-family: Arial, Helvetica, sans-serif;}
 
-/* Full-width input fields */
-input[type=text], input[type=password] {
-    width: 100%;
-    padding: 12px 20px;
-    margin: 8px 0;
-    display: inline-block;
-    border: 1px solid #ccc;
-    box-sizing: border-box;
-}
-
-/* Set a style for all buttons */
-button {
-    background-color: #4CAF50;
-    color: white;
-    padding: 14px 20px;
-    margin: 8px 0;
-    border: none;
-    cursor: pointer;
-    width: 100%;
-}
-
-button:hover {
-    opacity: 0.8;
-}
-
-/* Extra styles for the cancel button */
-.cancelbtn {
-    width: auto;
-    padding: 10px 18px;
-    background-color: #f44336;
-}
-
-/* Center the image and position the close button */
-.imgcontainer {
-    text-align: center;
-    margin: 24px 0 12px 0;
-    position: relative;
-}
-
-.imgavatar {
-    width: 40%;
-    border-radius:50%;
-}
-
-.container {
-    padding: 16px;
-}
-
-span.psw {
-    float: right;
-    padding-top: 16px;
-}
-
-/* The Modal (background) */
-.modal {
-    display:none; /* Hidden by default */
-    position: fixed; /* Stay in place */
-    z-index: 1; /* Sit on top */
-    left: 0;
-    top: 0;
-    width: 100%; /* Full width */
-    height: 100%; /* Full height */
-    overflow: auto; /* Enable scroll if needed */
-    background-color: rgb(0,0,0); /* Fallback color */
-    background-color: rgba(0,0,0.4); /* Black w/ pacity */
-    padding-top: 60px;
-}
-
-/* Modal Content/Box */
-.modal-content {
-    background-color: #fefefe;
-    margin: 5% auto 15% auto; /* 5% from the top, 15% from the bottom and centered */
-    border: 1px solid #888;
-    width: 80%; /* Could be more or less, depending on screen size */
-}
-
-/* The close button (x) */
-.close {
-    position:absolute;
-    right: 25px;
-    top: 0;
-    color: #000;
-    font-size: 35px;
-    font-weight: bold;
-}
-
-.close:hover,
-.close:focus {
-    color:red;
-    cursor: pointer;
-}
-
-/* Add Zoom Animation */
-.animate {
-    -webkit-animation: animatezoom 0.6s;
-    animation: animatezoom 0.6s
-}
-
-@-webkit-keyframes animatezoom {
-    from {-webkit-transform: scale(0)}
-    to {-webkit-transform: scale(1)}
-}
-
-@keyframes animatezoom {
-    from {transform: scale(0)}
-    to {transform: scale(1)}
-}
-
-/* Change styles for span and cancel button on extra small screens */
-@media screen and (max-width: 300px) {
-    span.psw {
-        display: block;
-        float: none;
-    }
-
-    .cancelbtn {
-        width: 100%
-    }
-}
 </style>
+
+<title>Login</title>
 </head>
 
 <body>
-<h2>Modal Login Form</h2>
+    <div class = "header">
+        <h1 align="center"><b>Welcome to the CERT Incident Management Tool (CIMT)</b></h1><br/>
+        <p align="center">The CIMT is an online web application that manages available resources and their assignments to various emergency incidents that may have already occured, are happening or may happen in the future in and around the campus. Emergency incidents may include, but not limited to, hazardous waste spills, acts of terrorism, nuclear incident, campus shooting, car crashes with fatalities, flooding, fire, etc.</p>
+    </div>
 
-<button onclick="document.getElementById('id01').style.display='block'" style="width:auto;">Login</button>
-
-<div id="id01"class="modal">
-
-    <form class="modal-content animate" action="/action_page.php">
-        <div class="imgcontainer">
-            <span onclick="document.getElementById('id01').style.display='none'" class="close" title="Close Modal">&times;</span>
-            <img src="img/img_avatar2.png" alt="Avatar" Class="avatar">
-        </div>
-
-        <div class="container">
-            <label for="uname"><b>Username</b></label>
-            <input type="text" placeholder="Enter Username" name="uname" required>
-
-            <label for="psw"><b>Username</b></label>
-            <input type="text" placeholder="Enter Password" name="psw" required>
-
-            <button type="submit">Login</button>
+    <div id="id01" class="login">
+    <h1>Login</h1>
+        <form>
+            <div class="form-group">
+                <label for="user">Username:</label>
+                <input type="text" class="form-control" id="user" placeholder="Enter Username" name="user" required>
+            </div>
+            <div class="form-group">
+                <label for="pw">Password: </label>
+                <input type="text" class="form-control" id="pw" placeholder="Enter Password" name="pw" required>
+            </div>
+            <button type="submit" class="btn btn-primary">Submit</button><br/>
             <label>
-                <input type="checkbox" checked="checked" name="remember"> Remember me
-            </label>
-        </div>
-
-        <div class="container" style="background-color:#f1f1f1">
-            <button type="button" onclick="document.getElementById('id01').style.display='none'" class="cancelbtn">Cancel</button>
-
-            <span class="psw">Forgot <a href="#">password?</a></span>
+                <input type="checkbox" checked="checked" name="remember">Remember me
+            </label><br/>
+            <span class="pw">Forgot <a href="#">password?</a></span>
         </div>
     </form>
 </div>
 
-<script>
-// Get the modal
-var modal = document.getElementById('id01');
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
-}
-</script>
-
 </body>
 </html>
+
+
--- src/routes/web.php
+++ src/routes/web.php
@@ -11,9 +11,7 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::redirect('/', 'login');
 
 Route::get('/login', function () {
     return view('login');
@@ -25,4 +23,39 @@ Route::get('/bootstrap', function () {
 
 Route::get('/bootstrapgrids', function () {
     return view('bootstrapgrids');
+});
+
+Route::get('/login2', function () {
+    return view('login2');
+});
+
+Route::get('/main', function () {
+    return view('main');
+});
+
+Route::get('/main10', function () {
+    return view('main10');
+});
+Route::get('/main11', function () {
+    return view('main11');
+});
+
+Route::get('/loginpage', function () {
+    return view('loginpage');
+});
+
+Route::get('/main2', function() {
+    return view('main2');
+});
+Route::get('/mainh', function() {
+    return view('mainhorizontal');
+});
+Route::get('/mains', function() {
+    return view('mainsticky');
+});
+Route::get('/mainr', function() {
+    return view('mainresponsive');
+});
+Route::get('/maind', function() {
+    return view('maindropdown');
 });
