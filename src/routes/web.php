4be0bec5ce4bc6d61f121c1b8ecb88e00b72f466
joe <joemoondev@gmail.com>
2019-04-19 00:22:10 -0700

    Cleaned up

--- src/routes/web.php
+++ src/routes/web.php
@@ -17,45 +17,6 @@ Route::get('/login', function () {
     return view('login');
 });
 
-Route::get('/bootstrap', function () {
-    return view('bootstrap');
-});
-
-Route::get('/bootstrapgrids', function () {
-    return view('bootstrapgrids');
-});
-
-Route::get('/login2', function () {
-    return view('login2');
-});
-
 Route::get('/main', function () {
     return view('main');
-});
-
-Route::get('/main10', function () {
-    return view('main10');
-});
-Route::get('/main11', function () {
-    return view('main11');
-});
-
-Route::get('/loginpage', function () {
-    return view('loginpage');
-});
-
-Route::get('/main2', function() {
-    return view('main2');
-});
-Route::get('/mainh', function() {
-    return view('mainhorizontal');
-});
-Route::get('/mains', function() {
-    return view('mainsticky');
-});
-Route::get('/mainr', function() {
-    return view('mainresponsive');
-});
-Route::get('/maind', function() {
-    return view('maindropdown');
 });
